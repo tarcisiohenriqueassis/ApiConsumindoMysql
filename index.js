20372f64dacc4fb029c088bddbfad66ac01c6db0
Your Name <you@example.com>
2024-11-23 21:29:26 -0300

    projeto  terminado

--- index.js
+++ index.js
@@ -1,22 +1,53 @@
 import express from 'express';
 
-import retornaListaTimes from './service/service.js';
+import {retornaListaTimes,retornaTimeID,retornaTimePorAno,retornaTimePeloNome} from './service/service.js';
+
 
 const app = express();
 
-app.get('/times',async ( req, res) =>{
 
-    let resultado = await retornaListaTimes(); 
+app.get('/times',async (req,res)=>{
 
-    if(resultado.length > 0){
-        res.json(resultado)
+    let ano = req.query.ano;
+    let time = req.query.time;
+    
+    let resultadoPesquisa;
 
+    if(typeof ano === 'undefined' && typeof time === 'undefined'){
+        resultadoPesquisa = await retornaListaTimes();
+    }
+    else if(typeof ano !== 'undefined'){
+        resultadoPesquisa = await retornaTimePorAno(parseInt(ano));
+    }
+    else if(typeof time !== 'undefined'){
+        resultadoPesquisa = await retornaTimePeloNome(time);
+    }
+
+    if(resultadoPesquisa.length > 0){
+        res.json(resultadoPesquisa)
     }
     else{
-        res.status(404).json({erro: 'erro'})
+        res.status(404).json({mensagem : 'nenhum time encontrado' })
     }
 
+})
+
+app.get('/times/:id',async ( req, res) =>{
+
+    let id = parseInt(req.params.id);
+    let resultadoBuscaID = await retornaTimeID(id);
+
+    if(resultadoBuscaID.length > 0){
+        res.json(resultadoBuscaID);
+    }
+    else if(isNaN(id)){
+        res.status(404).json({erro :'numero invalido'})
+    }
+    else{
+        res.status(404).json({ mensagem : 'id não encontrado'})
+    }
 });
+
 app.listen(3000, () =>{
     
     let data = new Date();
--- service/service.js
+++ service/service.js
@@ -1,16 +1,60 @@
 import pool from "../conexão/conexao.js";
 
 
+ async function executaQuery(conexao,query) {
+  
+   const resultado_Query = await conexao.query(query);
+   const resposta = resultado_Query[0];
+
+   return resposta;
+ }
+
+
  async function retornaListaTimes() {
     
     const conexao = await pool.getConnection();
+    const query ="SELECT id,campeao,vice,ano FROM times";
+    
+    const resposta = executaQuery(conexao,query)
 
-    const Campeao_tb = conexao.query('SELECT campeao, vice FROM bd_times.times');
-    const Campeao = Campeao_tb[0];
+    conexao.release();
+    
+    return resposta;
+  };
+
+  async function retornaTimeID(id) {
+    
+    const conexao = await pool.getConnection();
+    const query ='SELECT id,campeao,vice,ano FROM times WHERE id ='+id;
+    
+    const resposta = executaQuery(conexao,query);
 
     conexao.release();
 
-    return Campeao
-};
+    return resposta;
+  };
 
-export default retornaListaTimes;
+  async function retornaTimePorAno(ano){
+
+     const conexao = await pool.getConnection();
+     const query = 'SELECT id,campeao,vice, ano FROM times WHERE ano ='+ano; 
+     
+     const resultado = executaQuery(conexao,query);
+
+     conexao.release();
+
+     return resultado;
+  };
+
+  async function retornaTimePeloNome(nome) {
+    
+    const conexao = await pool.getConnection();
+    const query = 'SELECT id,campeao FROM times WHERE campeao ="'+nome+'"';
+
+    const resultado = executaQuery(conexao,query);
+
+    conexao.release();
+
+    return resultado;
+  };
+export  {retornaListaTimes, retornaTimeID,retornaTimePorAno, retornaTimePeloNome}; 
